397f76e1cece5c7e3485db5c714d7fb8fac49f37
Gianluca <gl.galassini@gmail.com>
2021-07-05 23:45:32 +0200

    Fine con Bonus: tutto è bene quel che finisce bene

---
+++ proj/src/components/Hero.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <div class="shade">
+        <img src="../assets/poke-jumbo.jpg" alt="">
+        <h2>Benvenuti!</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+}
+</script>
+
+<style scoped>
+img {
+    max-width:100%;
+}
+
+.shade {
+    width: 50%;
+    margin: auto;
+    position: relative;
+}
+
+h2 {
+    position: absolute;
+    top:0;
+    left: 50%;
+    transform: translateX(-50%);
+    color:#41b883;
+    font-size: 2rem;
+    text-shadow: 1px 2px #000;
+}
+</style>
